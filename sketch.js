8d2577b5dda394b12a74cf649b70ef4aa9274ce7
gregorima <107274044+gregorima@users.noreply.github.com>
2023-02-17 16:21:26 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,6 +10,8 @@ var playButton
 
 var player;
 
+var obstaclesGroup;
+
 var gameState = 0;
 
 function preload() {
@@ -34,13 +36,19 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   playButton = createButton("Jogar")
-  
-  
+  player = createSprite(width/2, height-300)
+  player.addImage(vacuumerImg)
+  player.scale = 0.25
+  player.setVelocity(0,-2)
+
+  obstaclesGroup = new Group();
+
+  createObstacles()
 }
 
 function draw() {
   background(startImg); 
-
+   
  if(gameState === 0) {
   playButton.position(windowWidth/2, windowHeight/2)
   playButton.class("customButton")
@@ -50,9 +58,47 @@ function draw() {
 
   
  }
-
+ 
  if(gameState === 1) {
   image(bgImg, 0, -height*5, width, height*6)
   playButton.hide()
+
+  if(keyIsDown(RIGHT_ARROW) && player.position.x<width-100) {
+    player.position.x +=10
+   }
+   if(keyIsDown(LEFT_ARROW) && player.position.x>100) {
+    player.position.x -=10
+    
+   }
+   if(keyIsDown(UP_ARROW) && player.position.x>100) {
+    player.position.y -=10
+   }
+
+   camera.position.y = player.position.y-100
+
+  drawSprites()
  }
-} 
+}
+
+function createObstacles() {
+  for(var i = 0; i<8; i++) {
+    var x = random(100, width-100)
+    var y = random(-height*4.5, height-400)
+    var obstacle = createSprite(x,y)
+    var randoimg = Math.round(random(1,3))
+    switch(randoimg) {
+      case 1: obstacle.addImage(fish1Img); obstacle.scale = 0.35; 
+      obstacle.setVelocity(2,0.5); break;
+      case 2: obstacle.addImage(fish2Img);
+       obstacle.scale = 0.07;
+       obstacle.setVelocity(-2,-0.5);
+       break;
+      case 3: obstacle.addImage(fish3Img); 
+      obstacle.scale = 0.07;
+      obstacle.setVelocity(-1,-0.5); break;
+    }
+    obstacle.addToGroup(obstaclesGroup)
+  }
+}
+
+
